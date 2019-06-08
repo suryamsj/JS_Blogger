cc4894203c434223e93db90844213661b5dcbcd7
suryamsj <50776952+suryamsj@users.noreply.github.com>
2019-06-08 09:52:58 +0700

    Update iklanbangsad.js

--- iklanbangsad.js
+++ iklanbangsad.js
@@ -131,7 +131,7 @@ function auto_safeconvert() {
         }
         if (a_to_vh == false) {
             var encryptedUrl = Base64.encode(a_to_vi);
-            a_to_ve[i].href = "http://safelinksugoi.blogspot.com/p/go.html?url=" + encryptedUrl;
+            a_to_ve[i].href = "https://safelinksugoi.blogspot.com/2019/06/sugoimusic.html?url=" + encryptedUrl;
             a_to_ve[i].rel = "nofollow";
             a_to_vb++;
             a_to_vc += i + ":::" + a_to_ve[i].href + "\n";
