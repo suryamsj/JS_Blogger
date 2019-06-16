45db777b8b01aa5f7c94cf47c074a399e008f345
suryamsj <50776952+suryamsj@users.noreply.github.com>
2019-06-16 10:33:56 +0700

    Add files via upload

---
+++ penting.js
@@ -0,0 +1,12 @@
+//<![CDATA[
+function loadCSS(e, t, n) { "use strict"; var i = window.document.createElement("link"); var o = t || window.document.getElementsByTagName("script")[0]; i.rel = "stylesheet"; i.href = e; i.media = "only x"; o.parentNode.insertBefore(i, o); setTimeout(function () { i.media = n || "all" }) }
+loadCSS("https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css");
+function showSearch(){
+  event.preventDefault()
+  document.getElementById('searchfs').classList.toggle('show')
+}
+function menuToggle() {
+document.getElementById('navigasi').classList.toggle('active')
+}
+eval(function(p,a,c,k,e,r){e=function(c){return(c<a?'':e(parseInt(c/a)))+((c=c%a)>35?String.fromCharCode(c+29):c.toString(36))};if(!''.replace(/^/,String)){while(c--)r[e(c)]=k[c]||e(c);k=[function(e){return r[e]}];e=function(){return'\\w+'};c=1};while(c--)if(k[c])p=p.replace(new RegExp('\\b'+e(c)+'\\b','g'),k[c]);return p}('7($("#2").8("9")!="a://b.c.d/"){$(\'e\').f(\'<3 g="h:i;4:0;j:0;z-k:l;m:n(0,0,0,0.6);o:p;5:1%;q-4:r%;s:1%;t-u:v;w-5:1%;x-y:A;">B, C D E 2 F</3>\')};',42,42,'|100|credit|div|top|height||if|attr|href|https|sugoimusicc|blogspot|com|body|html|style|position|fixed|left|index|99999|background|rgba|color|white|padding|20|width|text|align|center|line|font|size||30px|Tolong|jangan|di|hapus|linknya'.split('|'),0,{}))
+//]]>
